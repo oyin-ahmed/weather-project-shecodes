21987143e2b7c22ce78dc6603c6480aebd87c9b7
oyin-ahmed <oyink.ahmed@gmail.com>
2025-05-25 19:53:49 -0500

    added search functionality for temprature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+function updateTemprature(response) {
+  let tempEle = Math.round(response.data.temperature.current);
+  let degrees = document.querySelector(".degrees");
+  degrees.innerHTML = `${tempEle}`;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "603213a20od31054bbtafc903709b865";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateTemprature);
+}
 function changeCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = cityInput.value;
+
+  searchCity(cityInput.value);
 }
 let searchFrom = document.querySelector("#form");
 searchFrom.addEventListener("submit", changeCity);
